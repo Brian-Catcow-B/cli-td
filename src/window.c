9fd54b54df4d2522e48c98ac59c201dfde6658c4
Brian Billman <brianjbillman@gmail.com>
2022-08-14 23:01:31 -0400

    border reimplemented

--- src/window.c
+++ src/window.c
@@ -20,17 +20,22 @@ void init_window(window_t *self) {
 			init_window_tile(&(self->tiles[y][x]), pos2d(y, x));
 		}
 	}
+	window_generate_board_border(self);
 }
 
 void window_generate_board_border(window_t *self) {
 	for (size_t y = BOARD_BORDER_U - 1; y < BOARD_BORDER_U + BOARD_HEIGHT + 1; y++) {
 		window_tile_set_border(&(self->tiles[y][BOARD_BORDER_L - 1]), e_border_direction_vertical);
+		window_pos_update(self, pos2d(y, BOARD_BORDER_L - 1));
 		window_tile_set_border(&(self->tiles[y][BOARD_BORDER_L + BOARD_WIDTH]), e_border_direction_vertical);
+		window_pos_update(self, pos2d(y, BOARD_BORDER_L + BOARD_WIDTH));
 	}
 
 	for (size_t x = BOARD_BORDER_L - 1; x < BOARD_BORDER_L + BOARD_WIDTH + 1; x++) {
 		window_tile_set_border(&(self->tiles[BOARD_BORDER_U - 1][x]), e_border_direction_horizontal);
+		window_pos_update(self, pos2d(BOARD_BORDER_U - 1, x));
 		window_tile_set_border(&(self->tiles[BOARD_BORDER_U + BOARD_HEIGHT][x]), e_border_direction_horizontal);
+		window_pos_update(self, pos2d(BOARD_BORDER_U + BOARD_HEIGHT, x));
 	}
 }
 
